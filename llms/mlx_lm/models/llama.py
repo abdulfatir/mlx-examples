da64e976846919584ce326706793c5255375c6b3
Awni Hannun <awni@apple.com>
2024-03-13 08:55:41 -0700

    try no copy attention

--- llms/mlx_lm/models/llama.py
+++ llms/mlx_lm/models/llama.py
@@ -74,24 +74,31 @@ class Attention(nn.Module):
         queries, keys, values = self.q_proj(x), self.k_proj(x), self.v_proj(x)
 
         # Prepare the queries, keys and values for the attention computation
-        queries = queries.reshape(B, L, self.n_heads, -1).transpose(0, 2, 1, 3)
-        keys = keys.reshape(B, L, self.n_kv_heads, -1).transpose(0, 2, 1, 3)
-        values = values.reshape(B, L, self.n_kv_heads, -1).transpose(0, 2, 1, 3)
+        queries = queries.reshape(
+            B,
+            L,
+            self.n_kv_heads,
+            self.n_heads // self.n_kv_heads,
+            -1,
+        ).moveaxis(1, 3)
+        keys = keys.reshape(B, L, self.n_kv_heads, 1, -1).moveaxis(1, 3)
+        values = values.reshape(B, L, self.n_kv_heads, 1, -1).moveaxis(1, 3)
 
         if cache is not None:
             key_cache, value_cache = cache
-            queries = self.rope(queries, offset=key_cache.shape[2])
-            keys = self.rope(keys, offset=key_cache.shape[2])
-            keys = mx.concatenate([key_cache, keys], axis=2)
-            values = mx.concatenate([value_cache, values], axis=2)
+            queries = self.rope(queries, offset=key_cache.shape[-2])
+            keys = self.rope(keys, offset=key_cache.shape[-2])
+            keys = mx.concatenate([key_cache, keys], axis=-2)
+            values = mx.concatenate([value_cache, values], axis=-2)
         else:
             queries = self.rope(queries)
             keys = self.rope(keys)
 
-        output = mx.fast.scaled_dot_product_attention(
-            queries, keys, values, scale=self.scale, mask=mask
-        )
-        output = output.transpose(0, 2, 1, 3).reshape(B, L, -1)
+        scores = (queries * self.scale) @ keys.swapaxes(-1, -2)
+        if mask is not None:
+            scores += mask
+        scores = mx.softmax(scores.astype(mx.float32), axis=-1).astype(scores.dtype)
+        output = (scores @ values).moveaxis(3, 1).reshape(B, L, -1)
         return self.o_proj(output), (keys, values)
 
 
